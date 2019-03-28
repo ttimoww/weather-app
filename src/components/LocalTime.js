c94695bd2c63e8ed55acce1ca962a1c0e96ed48a
Timo <ttimoww@gmail.com>
2019-03-28 16:36:15 +0100

    Improved LocalTime component. Local time now has an active ticker.

--- src/components/LocalTime.js
+++ src/components/LocalTime.js
@@ -10,25 +10,27 @@ class LocalTime extends Component {
     }
 
     /**
-     * Get the local time of the city using google api
-     * @param {number} lat latitude of city
-     * @param {number} lon longitude of city
+     * Init the local time clock by getting the location's unix time
+     * and setting an interval
      */
-    getLocalTime(lat, lon){
-        let timestamp = new Date().getTime()/1000;
+    initClock(){
+        // Get unix
+        const timestamp = new Date().getTime()/1000;
         const appid = 'AIzaSyCo861UJhT1kb6yVfSbAFTK5td8hlLy6vA';
         fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${this.state.lat},${this.state.lon}&timestamp=${timestamp}&key=${appid}`)
-        .then(resp => resp.json())
-        .then((data) => {
-            console.log(timestamp + data.dstOffset + data.rawOffset);
-            this.setState({
-                localTime : timestamp + data.dstOffset + data.rawOffset
-            });
-        }, (error) => {
-            console.log(error);
-        })
+        .then(resp => resp.json())        
+        .then(data => this.setState({locationTimestamp: timestamp + data.dstOffset + data.rawOffset}));
+        
+        this.timestampTicker = setInterval(() => {
+            this.setState((prevState) => {
+                return{
+                    locationTimestamp: prevState.locationTimestamp += 1
+                }
+            })
+        }, 1000);
     }
 
+    
     /**
      * Converts unix to readable timestamp
      * @param {string} utc The unix value to be converted
@@ -42,13 +44,17 @@ class LocalTime extends Component {
     }
 
     componentDidMount(){
-        this.getLocalTime(this.state.lat, this.state.lon)
+        this.initClock();
+    }
+
+    componentWillUnmount(){
+        clearInterval( this.timestampTicker);
     }
 
     render() { 
         return ( 
             <div className="location__local-time">
-                <p>Local time: {this.convertToLocalTime(this.state.localTime)}</p>
+                <p>Local time: {this.convertToLocalTime(this.state.locationTimestamp)}</p>
             </div>
          );
     }
